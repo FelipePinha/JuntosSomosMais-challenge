5193cac480edb32a3835bd0da6325d46f90ce5d5
Felipe Carvalho <felipe_pc7@hotmail.com>
2024-02-21 11:31:42 -0300

    style: changed border width on input

--- src/components/ui/search-input.tsx
+++ src/components/ui/search-input.tsx
@@ -2,7 +2,7 @@ export const SearchInput = () => {
     return (
         <input
             type="text"
-            className="w-full py-1.5 rounded-full px-8 md:px-10 outline-none border-2 border-slate-200"
+            className="w-full py-1.5 rounded-full px-8 md:px-10 outline-none border border-slate-200"
             placeholder="Buscar aqui"
         />
     );
